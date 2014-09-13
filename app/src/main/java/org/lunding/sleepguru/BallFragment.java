3532537f687d1ca8ac9512f6f294cdf0ae1209a1
Julia <julia_wu@brown.edu>
2014-09-13 12:17:40 -0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	app/src/main/java/org/lunding/sleepguru/TestActivity.java

--- app/src/main/java/org/lunding/sleepguru/BallFragment.java
+++ app/src/main/java/org/lunding/sleepguru/BallFragment.java
@@ -2,8 +2,10 @@ package org.lunding.sleepguru;
 
 import android.app.Fragment;
 import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -28,25 +30,45 @@ public class BallFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_ball, container, false);
         final ImageButton ball = (ImageButton) view.findViewById(R.id.moving_ball);
 
+
         ball.setOnClickListener(new OnClickListener() {
+
+
             @Override
             public void onClick(View view) {
-                //difference between the start and current time is entered into array
-                times[hits] = System.currentTimeMillis() - startTime;
-                hits++;
-
-                switch (view.getId()){
-                    case (R.id.moving_ball):{
-                        Random r = new Random();
-                        int x = r.nextInt(480 + ball.getWidth());
-                        int y = r.nextInt(900 + ball.getHeight());
-
-                        ball.setX(x);
-                        ball.setY(y);
-                    }
+                if(hits==times.length){
+                    Intent intent = new Intent(new Intent(view.getContext(), ResultActivity.class));
+                    intent.putExtra("TIMES", times);
+                    startActivity(intent);
                 }
-                //restart timer
-                startTime = System.currentTimeMillis();
+                else{
+
+                    Display display = getActivity().getWindowManager().getDefaultDisplay();
+                    Point size = new Point();
+                    display.getSize(size);
+                    int width = size.x;
+                    int height = size.y;
+
+                    //difference between the start and current time is entered into array
+                    times[hits] = System.currentTimeMillis() - startTime;
+                    System.out.println("time stored: " + times[hits]);
+                    hits++;
+                    System.out.println("hits: " + hits);
+
+                    switch (view.getId()){
+                        case (R.id.moving_ball):{
+                            Random r = new Random();
+                            int x = r.nextInt(width - ball.getWidth());
+                            int y = r.nextInt(height - ball.getHeight());
+
+                            ball.setX(x);
+                            ball.setY(y);
+                        }
+                    }
+                    startTime = System.currentTimeMillis();
+                }
+
+                System.out.println("start time 2:" + startTime);
             }
         });
         return view;
